c31f5a3b1c08666036ed22db7b613f1ef995cefd
Audrey Carlsen <acarlsen@seattletimes.com>
2015-11-13 17:04:03 -0800

    second way to index

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ require("component-responsive-frame/child");
 
 var sound;
 
+var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
+
 // Event listeners
 document.querySelector(".play.with-sound").addEventListener("click", function(e) {
   sound = true;
@@ -27,14 +29,14 @@ var begin = function() {
 
     <div class="column">
       <div class="index mobile-index">
-        <i class="fa fa-circle dot-1"></i>
-        <i class="fa fa-circle dot-2"></i>
-        <i class="fa fa-circle dot-3"></i>
-        <i class="fa fa-circle dot-4"></i>
-        <i class="fa fa-circle dot-5"></i>
-        <i class="fa fa-circle dot-6"></i>
-        <i class="fa fa-circle dot-7"></i>
-        <i class="fa fa-circle dot-8"></i>
+        <i class="fa fa-circle dot-1" data-index="0"></i>
+        <i class="fa fa-circle dot-2" data-index="1"></i>
+        <i class="fa fa-circle dot-3" data-index="2"></i>
+        <i class="fa fa-circle dot-4" data-index="3"></i>
+        <i class="fa fa-circle dot-5" data-index="4"></i>
+        <i class="fa fa-circle dot-6" data-index="5"></i>
+        <i class="fa fa-circle dot-7" data-index="6"></i>
+        <i class="fa fa-circle dot-8" data-index="7"></i>
       </div>
 
       <div class="title"></div>
@@ -51,14 +53,14 @@ var begin = function() {
       <div class="readmore"><a href="
 http://www.seattletimes.com/seattle-news/puget-sound/ravensdale-marks-100-years-since-states-worst-mining-disaster/" target="_blank">Read the full story <i class="fa fa-long-arrow-right"></i></a></div>
       <div class="index desktop-index">
-        <i class="fa fa-circle dot-1"></i>
-        <i class="fa fa-circle dot-2"></i>
-        <i class="fa fa-circle dot-3"></i>
-        <i class="fa fa-circle dot-4"></i>
-        <i class="fa fa-circle dot-5"></i>
-        <i class="fa fa-circle dot-6"></i>
-        <i class="fa fa-circle dot-7"></i>
-        <i class="fa fa-circle dot-8"></i>
+        <i class="fa fa-circle dot-1" data-index="0"></i>
+        <i class="fa fa-circle dot-2" data-index="1"></i>
+        <i class="fa fa-circle dot-3" data-index="2"></i>
+        <i class="fa fa-circle dot-4" data-index="3"></i>
+        <i class="fa fa-circle dot-5" data-index="4"></i>
+        <i class="fa fa-circle dot-6" data-index="5"></i>
+        <i class="fa fa-circle dot-7" data-index="6"></i>
+        <i class="fa fa-circle dot-8" data-index="7"></i>
       </div>
 
 
@@ -67,9 +69,20 @@ http://www.seattletimes.com/seattle-news/puget-sound/ravensdale-marks-100-years-
 
   changeImage();
 
+  var onClickIndex = function() {
+    var newIndex = this.getAttribute("data-index");
+    index = newIndex;
+    changeImage();
+  }
+
+  qsa(".index i").forEach(function(dot) {
+    dot.addEventListener("click", onClickIndex);
+  });
+
   document.querySelector(".audio-icon").addEventListener("click", function(e) {
     if (sound) {
       document.querySelector(".audio-icon").classList.add("muted");
+      document.querySelector("audio").stop();
       sound = false;
     } else {
       document.querySelector(".audio-icon").classList.remove("muted");
@@ -79,8 +92,16 @@ http://www.seattletimes.com/seattle-news/puget-sound/ravensdale-marks-100-years-
 };
 
 var changeImage = function() {
-  document.querySelector(".desktop-index .dot-" + (index + 1)).classList.add("darker");
-  document.querySelector(".mobile-index .dot-" + (index + 1)).classList.add("darker");
+      console.log(index)
+  qsa(".index i").forEach(function(dot) {
+    var i = dot.getAttribute("data-index") * 1;
+    if (i <= index) {
+          console.log(dot)
+      dot.classList.add("darker");
+    } else {
+      dot.classList.remove("darker");
+    }
+  })
   document.querySelector(".title").innerHTML = `
     <div class="arrow-box"><i class="previous arrow fa fa-chevron-left"></i></div>
     <div class="featured">${data[index].featured}</div>
@@ -100,7 +121,7 @@ var changeImage = function() {
     document.querySelector(".buttons").classList.add("hidden");
     document.querySelector(".next").classList.add("hidden");
     document.querySelector(".rewind").classList.remove("hidden");
-    Array.prototype.slice.call(document.querySelectorAll(".index")).forEach(function(dot) {
+   qsa(".index").forEach(function(dot) {
       dot.classList.add("hidden");
     });
     document.querySelector(".readmore").classList.add("hidden");
@@ -108,19 +129,12 @@ var changeImage = function() {
     document.querySelector(".buttons").classList.remove("hidden");
     document.querySelector(".next").classList.remove("hidden");
     document.querySelector(".rewind").classList.add("hidden");
-    document.querySelector(".mobile-index .dot-" + (index + 2)).classList.remove("darker");
-    document.querySelector(".desktop-index .dot-" + (index + 2)).classList.remove("darker");
-    Array.prototype.slice.call(document.querySelectorAll(".index")).forEach(function(dot) {
+    qsa(".index").forEach(function(dot) {
       dot.classList.remove("hidden");
     });
     document.querySelector(".readmore").classList.remove("hidden");
   }
   if (index == 0) { 
-    Array.prototype.slice.call(document.querySelectorAll(".index .fa")).forEach(function(dot) {
-      dot.classList.remove("darker");
-    });
-    document.querySelector(".mobile-index .dot-" + (index + 1)).classList.add("darker");
-    document.querySelector(".desktop-index .dot-" + (index + 1)).classList.add("darker");
     document.querySelector(".previous").classList.add("disabled");
   } else {
     document.querySelector(".previous").classList.remove("disabled");
